eab41237eed793aa50920d22107175d518958a31
Sujimy <127483678+Sujimy@users.noreply.github.com>
2023-04-30 08:09:18 -1100

    Add files via upload

---
+++ MAHASISWA.java
@@ -0,0 +1,36 @@
+package LatihanM4;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class MAHASISWA {
+    String nama, kelas, matkulPraktikum;
+    int nim;
+
+    public MAHASISWA (String nm, String kl, String mat, int ni) {
+        nama = nm;
+        kelas = kl;
+        matkulPraktikum = mat;
+        nim = ni;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        HashMap<String, MAHASISWA>  mhs = new HashMap<>();
+        String input;
+        MAHASISWA data;
+
+        mhs.put("1", new MAHASISWA("Putri", "3H", "Struktur data", 2020000));
+        mhs.put("2", new MAHASISWA("Agus", "3A", "Matematika", 2020012));
+        mhs.put("3", new MAHASISWA("Arro", "3O", "Pemrograman", 2020017));
+
+        System.out.println("Masukkan ID: ");
+        input = in.nextLine();
+        data = mhs.get(input);
+        if (data != null) {
+            System.out.println("Data Mahasiswa : " + data.nama + ", Kelas : " +
+                    data.kelas + ", Mata Kuliah praktikum : " + data.matkulPraktikum +
+                    ", Nim : " + data.nim);
+        }
+    }
+}
